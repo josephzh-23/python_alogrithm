1dde8a45b30c301440b6d74e496a0c662d17ef20
Joseph Zhou <josephzh23@hotmail.com>
2024-11-17 13:58:35 -0800

    sdfs

---
+++ Graph/templates/preprocess_of_edges.py
@@ -0,0 +1,9 @@
+# Edges
+edges = [[0, 1], [1, 2], [2, 3], [4, 5], [5, 1], [5, 2]]
+
+# Graph represented as an adjacency list
+adj = [[] for _ in range(n)]
+
+# Constructing adjacency list
+for edge in edges:
+    adj[edge[0]].append(edge[1])
